ecaf53bf0518d9ebb3ffb76c5d83d7444ce53471
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2025-03-05 19:55:25 +0100

    Refactor: Separate brand queries into a dedicated module
    - Moved CRUD operations for the 'brand' entity to a new 'brandQueries.mjs'
      module
    - Updated 'db.mjs' to import queries from the new module
    - Improved code organization and modularity

---
+++ src/database/brandQueries.mjs
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient({ errorFormat: "pretty" });
+
+export const brand = {
+  // CRUD operations for the "brand" model
+  create: async (name) => {
+    console.log("hello");
+    try {
+      return await prisma.brand.create({ data: { name } });
+    } catch (e) {
+      throw new Error(`Failed to create brand: ${e.message}`);
+    }
+  },
+  getById: async (id) => {
+    try {
+      return await prisma.brand.findUnique({ where: { id } });
+    } catch (e) {
+      throw new Error(`Failed to get brand: ${e.message}`);
+    }
+  },
+  getByName: async (name) => {
+    try {
+      return await prisma.brand.findUnique({ where: { name } });
+    } catch (e) {
+      throw new Error(`Failed to get brand: ${e.message}`);
+    }
+  },
+  update: async (id, newName) => {
+    try {
+      return await prisma.brand.update({
+        where: { id },
+        data: { name: newName },
+      });
+    } catch (e) {
+      throw new Error(`Failed to update brand: ${e.message}`);
+    }
+  },
+  remove: async (id) => {
+    try {
+      await prisma.brand.delete({ where: { id } });
+    } catch (e) {
+      throw new Error(`Failed to delete brand: ${e.message}`);
+    }
+  },
+};
